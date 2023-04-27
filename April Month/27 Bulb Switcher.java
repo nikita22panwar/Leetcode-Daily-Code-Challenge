ec5ac9fc6649d170e989727042adc6f67ce4de4a
Nikita Panwar <78353504+nikita22panwar@users.noreply.github.com>
2023-04-27 21:58:54 +0530

    Create 27 Bulb Switcher.java

---
+++ April Month/27 Bulb Switcher.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        // return (int)Math.sqrt(n);
+
+
+        long low=0;
+        long high=n;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            if((mid*mid)==n){
+                return (int) mid;
+            }
+            else if((mid*mid)<n){
+                low= mid+1;
+            }
+            else{
+                high= mid-1;
+            }
+        }
+            return (int)low-1;
+        
+    }
+}
